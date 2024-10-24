f798f8268c9387be8151b4160bd95b24cf95fed0
mforgon <chatforgon@gmail.com>
2024-10-25 03:41:47 +0700

    change about us ui

--- lib/about_us_page.dart
+++ lib/about_us_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_with_firebase/home_page.dart';
+import 'package:flutter_with_firebase/profile_page.dart';
 import 'navigation_widget/common_bottom_navigationbar.dart';
 
 class AboutUsPage extends StatefulWidget {
@@ -11,10 +13,19 @@ class AboutUsPage extends StatefulWidget {
 class _AboutUsPageState extends State<AboutUsPage> {
   int _selectedIndex = 2; // Set the initial index to 2 for "About Us"
 
+  final List<Widget> _pages = [
+    const HomePage(),
+    const ProfilePage(),
+    const AboutUsPage(),
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      // Add navigation logic here if needed
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => _pages[index]),
+      );
     });
   }
 
@@ -24,54 +35,66 @@ class _AboutUsPageState extends State<AboutUsPage> {
       appBar: AppBar(
         title: const Text('About Us'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(
+                      'https://media.istockphoto.com/id/1360092910/photo/words-with-about-us-web-concept-idea.jpg?s=612x612&w=0&k=20&c=TyTppcG3XxtU8Oc8C9O455Lnc0auZHlPEOJBuMDzFBE='),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
             Text(
               'Our Mission',
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16.0),
-            Text(
+            const SizedBox(height: 16.0),
+            const Text(
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, enim eget tristique mattis, sem elit lacinia nibh, nec malesuada nisi metus vel nunc. Sed id nisi eget nisl lacinia ultricies.',
               style: TextStyle(fontSize: 16.0),
             ),
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
             Text(
               'Our Team',
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16.0),
-            Row(
+            const SizedBox(height: 16.0),
+            Column(
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/team_member1.jpg'),
+                _buildTeamMember(
+                  'John Doe',
+                  'CEO',
+                  'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg',
                 ),
-                SizedBox(width: 16.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('John Doe'),
-                    Text('CEO'),
-                  ],
+                _buildTeamMember(
+                  'Jane Smith',
+                  'Product Manager',
+                  'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
                 ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/team_member2.jpg'),
+                _buildTeamMember(
+                  'Jane Doam',
+                  'CEO',
+                  'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg',
                 ),
-                SizedBox(width: 16.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Jane Smith'),
-                    Text('Product Manager'),
-                  ],
+                _buildTeamMember(
+                  'Jane Smith',
+                  'Product Manager',
+                  'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
                 ),
               ],
             ),
@@ -81,6 +104,27 @@ class _AboutUsPageState extends State<AboutUsPage> {
       bottomNavigationBar: CommonBottomNavigationBar(
         selectedIndex: _selectedIndex,
         onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildTeamMember(String name, String position, String imageUrl) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(imageUrl),
+          ),
+          const SizedBox(width: 16.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(name),
+              Text(position),
+            ],
+          ),
+        ],
       ),
     );
   }
